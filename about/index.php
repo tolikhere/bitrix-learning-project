7e6a40fa40504703438bf43fe84a5ce84ca3e807
Tolik Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-10-28 14:35:59 +0300

    Create page about

---
+++ about/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('О нас');
+$APPLICATION->SetPageProperty('TITLE', 'О нас | We Coders');
+
+$APPLICATION->SetPageProperty("keywords", "о нас, we coders, сайты");
+$APPLICATION->SetPageProperty("description", "О нашей компании");
+?> 
+<p>О нас</p>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
